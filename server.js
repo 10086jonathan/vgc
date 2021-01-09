73019e62f8edbc838676adaa9917ab96af976a2d
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-09 09:19:09 -0800

    adds package.json

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const logger = require('morgan');
+// const cors = require('cors');
+
+// variables
+const app = express();
+const port = process.env.PORT || 3001;
+// const usersRouter = require('./routes/api/users');
+
+require('dotenv').config();
+require('./config/database');
+
+
+// middleware
+app.use(logger('dev'));
+app.use(express.json());
+// app.use(cors());
+
+// routes
+// app.use('/api/users', usersRouter);
+
+app.listen(port, function() {
+    console.log(`express app is running on port ${port}`)
+});
