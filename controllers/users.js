f05770b04af2c422be57f66a99b69acc5df443f3
jon apagueno <jonathan.apagueno@gmail.com>
2021-01-12 17:25:38 -0800

    requires email at model and cleans up controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,45 @@
 // modules
 const User = require('../models/user');
-// const SECRET = process.env.SECRET;
-// const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
+const jwt = require('jsonwebtoken');
 
 // exports
 module.exports = {
-    signup,
+    signup
 };
 
 // controller action
 async function signup(req, res) {
+
+    console.log(req.body);
+
+    
     try {
         const user = await User.create(req.body);
 
-        // const token = createJWT(user);
+        const token = createJWT(user);
 
-        res.json({ user });
+        res.json({ token });
 
-    } catch (error) {
-        res.status(400).json({ error });
+    } catch (err) {
+      // probably a duplicate email
+        console.log(err);
+
+        res.status(400).json(err);
     }
+  }
+
+// async function login(req, res) {
+//     try {
+//         const user = await User.findOne({ email: req.body.emai });
+//         if(!user) return res.status(401).json({ err: 'bad credentials' });
+
+//     } catch (error) {
+        
+//     }
+// }
+
+function createJWT(user) {
+    console.log(SECRET)
+    return jwt.sign({ user }, SECRET, {expiresIn: '24h'})
 }
